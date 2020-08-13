c981226c176b56e31c348003860e178a0e7becf4
Валерий К <kors.vv@ya.ru>
2020-08-13 14:58:29 +0400

    +

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Задачи интерволга</title>
+</head>
+<body>
+	<h2><a href="snake_array/">массив змейкой</a></h2>
+</body>
+</html>
---
+++ snake_array/index.php
@@ -0,0 +1,36 @@
+<?php
+require("class.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Массив змейкой</title>
+</head>
+
+<body>
+	<header class="header">
+		<div class="overlay"></div>
+		<div class="container">
+		<div class="description text-center">
+			<h3>
+				<font color="#3AC1EF">▍
+					Hello ,Welcome To My officail Website
+					<p>
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<button class="btn btn-outline-secondary">See more</button>
+				</font>
+			</h3>
+		</div>
+		</div>
+
+
+	</header>
+</body>
+
+</html>
